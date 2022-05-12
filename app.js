50caa7a092b100f2ef8d4ea387828b5c47786818
pedro <pedro.jose.bidoggia@hotmail.com>
2022-05-11 22:03:52 -0300

    position, transition and transform

--- app.js
+++ app.js
@@ -11,16 +11,18 @@ const public = path.join(__dirname, 'public/')
 // hhtp routes
 const httpRaiz = '/'
 const httpHome = '/home'
-const httpFlexBox = '/flexbox'
 const httpRegister = '/register'
 const httpLogin = '/login'
 const httpHomeLogin = '/homelogin'
+const httpFlexBox = '/flexbox'
+const httpPosition = '/position'
 
 // html
 const homeHtml = 'home.html'
-const flexBoxHtml = 'flexbox.html'
 const registerHtml = 'register.html'
 const loginHtml = 'login.html'
+const flexBoxHtml = 'flexbox.html'
+const positionHtml = 'position.html'
 
 // Define the static file path
 app.use(express.static(__dirname +'/public/'));
@@ -33,23 +35,26 @@ app.get(httpHome, (req, res) => {
     res.sendFile(path.join(views, homeHtml))
 })
 
-app.get(httpFlexBox, (req, res) => {
-    res.sendFile(path.join(views, flexBoxHtml))
-})
-
 app.get(httpRegister, (req, res) => {
     res.sendFile(path.join(views, registerHtml))
 })
 
-app.get(httpHomeLogin, (req, res) => {
+app.get(httpLogin, (req, res) => {
     res.sendFile(path.join(views, loginHtml))
 })
-app.post(httpLogin, (req, res) => {
+app.post(httpHomeLogin, (req, res) => {
     res.sendFile(path.join(views, loginHtml))
+})
+
+app.get(httpFlexBox, (req, res) => {
+    res.sendFile(path.join(views, flexBoxHtml))
+})
+app.get(httpPosition, (req, res) => {
+    res.sendFile(path.join(views, positionHtml))
 })
 
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`App listening at http://localhost:${port}`)
     console.log(__dirname)
 })
